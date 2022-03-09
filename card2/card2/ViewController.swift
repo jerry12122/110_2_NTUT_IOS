f99016974913e14ae5396d9d49aa0f431b34c5a9
jerry12122 <a0975844268@gmail.com>
2022-03-10 01:40:56 +0800

    card2

---
+++ card2/card2/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  card2
+//
+//  Created by student on 2022/3/9.
+//  Copyright © 2022年 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var filps_view: UILabel!
+    var flips:Int = 0
+    {
+        didSet{
+            filps_view.text = "Flips:\(flips)"
+        }
+    }
+    var emoji_choices = ["🤡","💩"]
+    @IBOutlet var btn_collection: [UIButton]!
+    
+    @IBAction func flipCard(_ sender: UIButton) {
+        if let cardNumber = btn_collection.firstIndex(of: sender){
+            print("cardNumber = \(String(describing: cardNumber))")
+            if sender.currentTitle == emoji_choices[cardNumber]{
+                sender.setTitle("", for: UIControl.State.normal)
+                sender.backgroundColor = #colorLiteral(red: 0.8505474329, green: 0.8489837646, blue: 0.9567221999, alpha: 1)
+            }else{
+                sender.setTitle(emoji_choices[cardNumber], for: UIControl.State.normal)
+                sender.backgroundColor = #colorLiteral(red: 0.9572871327, green: 0.9418804049, blue: 0.8999911547, alpha: 1)
+            }
+            flips+=1
+        }
+    }
+    /*
+    @IBAction func cCard(_ sender: UIButton) {
+        if sender.currentTitle == "🤡"{
+            sender.setTitle("", for: UIControlState.normal)
+            sender.backgroundColor = #colorLiteral(red: 0.8505474329, green: 0.8489837646, blue: 0.9567221999, alpha: 1)
+        }else{
+            sender.setTitle("🤡", for: UIControlState.normal)
+            sender.backgroundColor = #colorLiteral(red: 0.9572871327, green: 0.9418804049, blue: 0.8999911547, alpha: 1)
+        }
+        flips+=1
+    }
+    @IBAction func pCard(_ sender: UIButton) {
+        if sender.currentTitle == "💩"{
+            sender.setTitle("", for: UIControlState.normal)
+            sender.backgroundColor = #colorLiteral(red: 0.8505474329, green: 0.8489837646, blue: 0.9567221999, alpha: 1)
+        }else{
+            sender.setTitle("💩", for: UIControlState.normal)
+            sender.backgroundColor = #colorLiteral(red: 0.9572871327, green: 0.9418804049, blue: 0.8999911547, alpha: 1)
+        }
+        flips+=1
+    }
+    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
