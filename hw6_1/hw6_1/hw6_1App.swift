b3ea2e0a0cafcc32262f6e0c66cb24fb53c2c875
jerry12122 <a0975844268@gmail.com>
2022-05-05 19:30:56 +0800

    hw6

---
+++ hw6_1/hw6_1/hw6_1App.swift
@@ -0,0 +1,17 @@
+//
+//  hw6_1App.swift
+//  hw6_1
+//
+//  Created by jerry on 2022/5/5.
+//
+
+import SwiftUI
+
+@main
+struct hw6_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
