da6817540c2614484d3fcb2884f702afa4ce5a0c
jerry12122 <a0975844268@gmail.com>
2022-06-20 13:07:16 +0800

    lab12

---
+++ lab11/lab11/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  lab11
+//
+//  Created by jerry on 2022/6/8.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneView.delegate = self
+        sceneView.automaticallyUpdatesLighting = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "ARKit_Class_QRCode", bundle: Bundle.main){
+            configuration.trackingImages = trackingImages
+            configuration.maximumNumberOfTrackedImages = 1
+        }
+        
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+        if let imageAnchor = anchor as? ARImageAnchor {
+            let size = imageAnchor.referenceImage.physicalSize
+            let plane = SCNPlane(width: size.width, height: size.height)
+            plane.firstMaterial?.diffuse.contents = UIColor.white.withAlphaComponent(0.5)
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -.pi / 2
+            node.addChildNode(planeNode)
+            
+            var shapeNode: SCNNode?
+            if imageAnchor.referenceImage.name == "ARKit_Class_QRCode"{
+                //shapeNode = dragonNode
+            }
+            guard let shape = shapeNode else { return nil}
+            node.addChildNode(shape)
+        }
+        
+        return node
+        
+    }
+    
+}
